edba60468c453c41138f840ca87e8c3910b45f38
JulioCesar69 <juliocrack912@gmail.com>
2022-08-19 14:49:31 -0500

    Concatenación

---
+++ Leccion02/02-01-Concatenar.js
@@ -0,0 +1,17 @@
+var nombre = "Julio Cesar";
+var apellido = "López Sanjuan";
+
+var nombreCompleto = nombre + " " + apellido;
+console.log(nombreCompleto);
+
+var nombreCompleto2 = "Julio " + "López";
+console.log(nombreCompleto2);
+
+var x = nombre + 120;
+console.log(x);
+
+x = nombre + (2 + 4);
+console.log(x);
+
+x = 2 + 4 + nombre;
+console.log(x);
