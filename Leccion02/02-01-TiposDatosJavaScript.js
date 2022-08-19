1d4c292d6928f7eedf19c87fa8da790e6a712fcd
JulioCesar69 <juliocrack912@gmail.com>
2022-08-19 13:59:29 -0500

    Tipos de datos

---
+++ Leccion02/02-01-TiposDatosJavaScript.js
@@ -0,0 +1,15 @@
+//tipo de dato string 
+var nombre = "Julio";
+console.log(nombre);
+
+//tipo de dato numerico
+var numero = 1000;
+console.log(numero);
+
+//tipo de dato object
+var objeto = {
+    nombre: "Julio",
+    apellido: "Lopez",
+    telefono: "9530000000"
+}
+console.log(objeto);
