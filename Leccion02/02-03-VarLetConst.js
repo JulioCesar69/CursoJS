c86e00979f59e9f9467a68414139a2eee9a69a7f
JulioCesar69 <juliocrack912@gmail.com>
2022-08-20 14:13:51 -0500

    Más ejemplos variables

--- Leccion02/02-03-VarLetConst.js
+++ Leccion02/02-03-VarLetConst.js
@@ -1,6 +1,15 @@
-let nombre;
-nombre = "juan";
-console.log(typeof nombre);
+let nombreCompleto = "Julio López";
+let nombrecompleto = "Juan Perez";
+console.log( nombreCompleto );
+console.log(nombrecompleto );
 
-const apellido = "perez";
-apellido = "lara"; 
+let x, y;
+x = 10, y = 20;
+let z = x + y;
+console.log(z);
+
+let a1nombre;
+let $1nombre;
+let _nombre;
+// let 1nombre; no se puede iniciar una variable con número
+//let break; no se pueden usar palabras reservadas para variables
