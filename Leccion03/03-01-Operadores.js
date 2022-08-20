89e0a5a1756ca8dacc324fe54e89ba7f52b79240
JulioCesar69 <juliocrack912@gmail.com>
2022-08-20 14:25:48 -0500

    Operadores

---
+++ Leccion03/03-01-Operadores.js
@@ -0,0 +1,18 @@
+let a = 3, b = 2;
+let suma = a + b;
+console.log( "resultado de la suma: " + suma );
+
+resta = a - b;
+console.log("resultado de la resta: " + resta);
+
+multi = a * b;
+console.log("resultado de la multiplicacion: " + multi);
+
+div = a / b;
+console.log("resultado de la division " + div);
+
+modulo = a % b;
+console.log("resultado de la operacion modulo: " + modulo);
+
+exponente = a ** b;
+console.log("resultado de la operacion exponente: " + exponente)
