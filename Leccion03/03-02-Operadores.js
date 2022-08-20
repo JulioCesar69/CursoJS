d73cabf68629dac49e3573559c398b86ec482343
JulioCesar69 <juliocrack912@gmail.com>
2022-08-20 14:38:25 -0500

    Incremento y decremento

---
+++ Leccion03/03-02-Operadores.js
@@ -0,0 +1,25 @@
+let a = 3, b = 2;
+let z = a + b;
+
+//incremento
+//pre-incremento (el operador ++ antes de la variable)
+z= ++a;
+console.log(a);
+console.log(z);
+
+//post-incremento (el operador ++ despues de la variable)
+z = b++;
+console.log(b);
+console.log(z);
+
+//Decremento
+//predecremento 
+z = --a;
+console.log(a);
+console.log(z);
+
+//postdecremento
+z = b--;
+console.log(b);
+console.log(z);
+
